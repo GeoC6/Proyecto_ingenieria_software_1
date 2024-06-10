69c0a96d24f25663b795ac37a6c4239350ed0358
GeoC <diego.cubilhos@gmail.com>
2024-06-10 03:27:26 -0400

    Cambio controlador webpay

--- server/src/controllers/webpayController.ts
+++ server/src/controllers/webpayController.ts
@@ -14,11 +14,15 @@ export const createTransaction = async (req: Request, res: Response): Promise<vo
 
 export const commitTransaction = async (req: Request, res: Response): Promise<void> => {
     const { token } = req.body;
-
     try {
         const response = await webpayService.commitTransaction(token);
-        res.status(200).json(response);
+        if (response.response_code === 0 && response.status === 'AUTHORIZED') {
+            res.status(200).json({ message: 'Transacción exitosa', details: response });
+        } else {
+            res.status(400).json({ error: 'Transacción no autorizada', details: response });
+        }
     } catch (error) {
-        res.status(500).json({ error: 'Failed to commit transaction' });
+        console.error('Error en commitTransaction:', error);
+        res.status(500).json({ error: 'Failed to commit transaction', details: error });
     }
 };
